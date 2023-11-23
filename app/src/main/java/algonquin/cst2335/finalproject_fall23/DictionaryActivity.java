d962b15a246e981c4db28b726612f145726727ec
KexinGwan <54641785+KexinGwan@users.noreply.github.com>
2023-11-23 12:19:37 -0500

    some changes

--- app/src/main/java/algonquin/cst2335/finalproject_fall23/DictionaryActivity.java
+++ app/src/main/java/algonquin/cst2335/finalproject_fall23/DictionaryActivity.java
@@ -25,6 +25,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import algonquin.cst2335.finalproject_fall23.databinding.DictionaryactivityMainBinding;
+import algonquin.cst2335.finalproject_fall23.databinding.PostsearchlayoutBinding;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -32,23 +33,17 @@ import retrofit2.Response;
 
 public class DictionaryActivity extends AppCompatActivity {
     DictionaryactivityMainBinding binding;
-    ArrayList<String> definitions = new ArrayList<>();
+    ArrayList<String> definitions;
     DefinitionViewModel definitionModel;
     DefinitionDAO mDAO;
     RecyclerView.Adapter myAdapter;
-    SharedPreferences prefs;
-    int selectedIndex;
+
+    //int selectedIndex;
 
     //if not using binding you can get the elements by this way.
     //EditText searchEditText = findViewById(R.id.editText);
     //EditText searchEditText = binding.editText;
     //String searchTerm = searchEditText.getText().toString();
-
-//    private void showToast(String errorMessage) {
-//        Toast.makeText(DictionaryActivity.this, errorMessage, Toast.LENGTH_SHORT).show();
-//    }
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,37 +52,47 @@ public class DictionaryActivity extends AppCompatActivity {
         //use binding to access views defined in the layout.
         binding = DictionaryactivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
+        Toast.makeText(this, "Welcome to Dictionary", Toast.LENGTH_LONG).show();
         EditText searchEditText = binding.editText;
-        String searchTerm = searchEditText.getText().toString();
 
+        //binding.editText = findViewById(R.id.editText);
+//        String searchTerm = searchEditText.getText().toString();
         //apply SharedPreference
-//        SharedPreferences prefs = getSharedPreferences("searchTerm", Context.MODE_PRIVATE);
-//        searchEditText.setText(prefs.getString("searchTerm", ""));
-        prefs=this.getSharedPreferences("com.yourapp.dictionary_preferences",MODE_PRIVATE);
-        searchTerm=prefs.getString("LastSearchWord","");
-        searchEditText.setText("searchTerm");
+        SharedPreferences prefs1 = getSharedPreferences("searchTerm", Context.MODE_PRIVATE);
+        searchEditText.setText(prefs1.getString("searchTerm", ""));
+
+        //prefs=this.getSharedPreferences("com.yourapp.dictionary_preferences",MODE_PRIVATE);
+//        searchTerm=prefs.getString("LastSearchWord","");
+//        searchEditText.setText("searchTerm");
 
         //open a database
         DefinitionDB db = Room.databaseBuilder(getApplicationContext(), DefinitionDB.class, "databaseLocal").build();
         mDAO = db.cmDAO();
 
         definitionModel = new ViewModelProvider(this).get(DefinitionViewModel.class);
-        definitions = definitionModel.definitions.getValue();
+        if(definitionModel.definitions.getValue()!=null){
+            definitions =definitionModel.definitions.getValue();}
+        else{definitions = new ArrayList<>();
+        }
 
         //click listener for the search button
         Button searchButton = findViewById(R.id.search_button);
         searchButton.setOnClickListener(v -> {
-            Log.d("DictionaryActivity", "Search Term: " + searchTerm);
-// load the last search term
-            //searchTerm = searchEditText.getText().toString();
+            String searchTerm = binding.editText.getText().toString();
             definitions.add(searchTerm);
+
             // save the last searched term
-            SharedPreferences.Editor editor=prefs.edit();
-            editor.putString("Last Search Term",searchTerm);
+            SharedPreferences prefs2=getSharedPreferences("searchTerm",Context.MODE_PRIVATE);
+
+            SharedPreferences.Editor editor=prefs1.edit();
+            editor.putString("searchTerm",searchTerm);
             editor.apply();
 
-            searchEditText.setText("");
+//            String searchTerm=prefs1.getString("LastSearchWord","");
+//            Log.d("DictionaryActivity", "Search Term: " + searchTerm);
+            //searchTerm = searchEditText.getText().toString();
+
+//            binding.editText.setText("");
             myAdapter.notifyItemInserted(definitions.size()-1);
 
 //            //use DictionaryApiService to connect the external url.
@@ -135,7 +140,7 @@ public class DictionaryActivity extends AppCompatActivity {
         Button saveButton = findViewById(R.id.save_button);
         saveButton.setOnClickListener(clk->{
             Definition newDefinition = new Definition();
-            newDefinition.setTerm(searchTerm);
+          //  newDefinition.setTerm(searchTerm);
 //            newDefinition.setDefinition(definition);
 //            long insertedId = cmDAO.insertDefinition(newDefinition);
 //
@@ -145,9 +150,7 @@ public class DictionaryActivity extends AppCompatActivity {
 //            } else {
 //                showToast("Failed to save definition");
 //            }
-
         });
-
 
         //click listener for the history button
          Button historyButton= findViewById(R.id.history_button);
@@ -163,14 +166,45 @@ public class DictionaryActivity extends AppCompatActivity {
 //             recyclerViewAdapter.notifyDataSetChanged();
 
          });
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        //myRowHolder
-        class MyRowHolder extends RecyclerView.ViewHolder {
-            EditText searchEditText = binding.editText;
-            //String definition;
-            public MyRowHolder(@NonNull View itemView) {
-                super(itemView);
-                itemView.setOnClickListener(clk -> {
+        // setAdapter here
+        binding.recyclerView.setAdapter(myAdapter= new RecyclerView.Adapter<MyRowHolder>() {
+//            TextView searched_term = binding.searched_term;
+//            String searchTerm = searched_term.getText().toString();
+            @NonNull
+            @Override
+            public MyRowHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                PostsearchlayoutBinding binding1 = PostsearchlayoutBinding.inflate(getLayoutInflater());
+                return new MyRowHolder(binding1.getRoot());
+            }
+            @Override
+            public void onBindViewHolder(@NonNull MyRowHolder holder, int position) {
+//                selectedIndex = position;
+//                String forRow = definitions.get(position).toString();
+                holder.searched_term.setText("forRow");
+            }
+            @Override
+            public int getItemCount() {
+                //return definitions.size();
+                return definitions != null ? definitions.size() : 0;
+            }
+//            @Override
+//            public int getItemViewType(int position){
+//                return 0;
+//            }
+        });// end of setAdapter
+
+    } //end of onCreate()
+
+    //myRowHolder
+    class MyRowHolder extends RecyclerView.ViewHolder {
+        TextView searched_term;
+        //String definition;
+        public MyRowHolder(@NonNull View itemView) {
+            super(itemView);
+            searched_term=itemView.findViewById(R.id.searched_term);
+            itemView.setOnClickListener(clk -> {
 
                 int position = getAdapterPosition();
                 String selectedTerm = definitions.get(position);
@@ -180,22 +214,12 @@ public class DictionaryActivity extends AppCompatActivity {
                 builder.setTitle("Question:");
                 builder.setNegativeButton("No",(dialog,cl)->{});
                 builder.setPositiveButton("Yes",(dialog,cl)->{
-
-//                    Executor thread = Executors.newSingleThreadExecutor();
-//                    thread.execute(new Runnable() {
-//                        @Override
-//                        public void run() {
-//                           mDAO.deleteMessage(m);
-//                        }
-//                    });
                     String  selectedDefinition = definitions.get(position);
                     definitions.remove(position);
                     myAdapter.notifyItemRemoved(position);
-//
-                    Snackbar.make(searchEditText,"You deleted message #"+ position, Snackbar.LENGTH_LONG).setAction("Undo", click->{
+                    Snackbar.make(searched_term,"You deleted message #"+ position, Snackbar.LENGTH_LONG).setAction("Undo", click->{
                         definitions.add(position, selectedDefinition);
                         myAdapter.notifyItemInserted(position);
-//
 //                        Executor thread1 = Executors.newSingleThreadExecutor();
 //                        thread1.execute(new Runnable() {
 //                            @Override
@@ -206,39 +230,8 @@ public class DictionaryActivity extends AppCompatActivity {
                     }).show();
                 });
                 builder.create().show();
-                });
-            }
-        } // end of MyRowHolder
-
-        binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        // setAdapter here 213-237
-        binding.recyclerView.setAdapter(myAdapter= new RecyclerView.Adapter<MyRowHolder>() {
-            EditText searchEditText = binding.editText;
-            String searchTerm = searchEditText.getText().toString();
-
-            @NonNull
-            @Override
-            public MyRowHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-                DictionaryactivityMainBinding binding = DictionaryactivityMainBinding.inflate(getLayoutInflater());
-                return new MyRowHolder(binding.getRoot());
-            }
-            @Override
-            public void onBindViewHolder(@NonNull MyRowHolder holder, int position) {
-                selectedIndex = position;
-                String forRow = definitions.get(position).toString();
-                holder.searchEditText.setText(forRow);
-            }
-            @Override
-            public int getItemCount() {
-                return definitions.size();
-            }
-            @Override
-            public int getItemViewType(int position){
-                return 0;
-            }
-        });// end of setAdapter
-    } //end of onCreate()
-
+            });
+        }
+    } // end of MyRowHolder
 
 }// end of the class
