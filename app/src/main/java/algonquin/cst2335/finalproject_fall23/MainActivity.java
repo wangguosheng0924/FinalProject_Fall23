88c99520c94d5e53e36f09be5ee88a8a34b1b7fd
KexinGwan <54641785+KexinGwan@users.noreply.github.com>
2023-12-05 16:36:15 -0500

    Christy finished merge again 12.05

--- app/src/main/java/algonquin/cst2335/finalproject_fall23/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/finalproject_fall23/MainActivity.java
@@ -30,6 +30,24 @@ import java.util.concurrent.Executors;
 
 import algonquin.cst2335.finalproject_fall23.databinding.ActivityMainBinding;
 
+/**
+ * DictionaryActivity represents the main activity for a dictionary app.
+ * It allows users to search for word definitions, save them locally, and view their search history.
+ *
+ * The activity includes functionality for:
+ * - Searching for word definitions using an online dictionary API.
+ * - Saving and deleting word definitions locally using a Room database.
+ * - Viewing and managing the search history.
+ * - Displaying word definitions in a RecyclerView.
+ *
+ * The activity also utilizes the ViewModel architecture for managing UI-related data.
+ *
+ * This class implements the HistoryFragment.HistoryClickListener interface to handle clicks
+ * on items in the search history.
+ *
+ * @author Christy Guan
+ * @version 1.0
+ */
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
@@ -58,6 +76,19 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+
+//    public void onDictionaryButtonClick(View view){
+//        Intent intent = new Intent(this, DictionaryActivity.class);
+//        startActivity(intent);
+//        binding.songButton.setOnClickListener(click -> {
+//            startActivity(new Intent(this, ArtistsSearch.class));
+//        });
+//
+//        //initialize the toolbar.Android will call onCreateOptionsMenu().
+//        setSupportActionBar(binding.mainToolbar);
+//    }
+//
     @Override //this initialized the toolbar
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.my_menu, menu);
@@ -82,43 +113,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public void onDictionaryButtonClick(View view){
-        Intent intent = new Intent(this, DictionaryActivity.class);
-        startActivity(intent);
-        binding.songButton.setOnClickListener(click -> {
-            startActivity(new Intent(this, ArtistsSearch.class));
-        });
-
-        //initialize the toolbar.Android will call onCreateOptionsMenu().
-        setSupportActionBar(binding.mainToolbar);
-
-    }
-    @Override //this initialized the toolbar
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.my_menu, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override // this defines when users select a menuItem
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-
-        switch( item.getItemId() )
-        {
-            case R.id.item_dictionary:
-                Snackbar.make(binding.mainToolbar,"You will go to the dictionary",Snackbar.LENGTH_LONG).show();
-                Intent intent = new Intent(this, DictionaryActivity.class);
-                startActivity(intent);
-                break;
-
-            case R.id.item_about:
-                Toast.makeText(this, "created by Christy", Toast.LENGTH_SHORT).show();
-        }
-        return true;
-    }
-
-
-    public void onDictionaryButtonClick(View view){
-        Intent intent = new Intent(this, DictionaryActivity.class);
-        startActivity(intent);
-    }
+//    public void onDictionaryButtonClick(View view){
+//        Intent intent = new Intent(this, DictionaryActivity.class);
+//        startActivity(intent);
+//    }
 }
